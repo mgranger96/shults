20e8ade0f0977c4e27e338b38540864e22435666
mgranger96 <michael.granger@gmail.com>
2013-05-20 10:37:35 -0300

    Update 20130515005711_add_user_id_to_updates.rb

--- db/migrate/20130515005711_add_user_id_to_updates.rb
+++ db/migrate/20130515005711_add_user_id_to_updates.rb
@@ -1,7 +1,3 @@
 class AddUserIdToUpdates < ActiveRecord::Migration
-  def change
-  	add_column :updates, :user_id, :integer
-  	add_index :updates, :user_id
-  	remove_column :updates
-  end
+
 end
